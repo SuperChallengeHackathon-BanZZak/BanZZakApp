554e03b6eeaf94cb7d2a6eda064dfb1b17095823
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-01-09 15:35:24 +0900

    cardview 체크 완료
    cardview 체크 완료

--- app/src/main/java/com/greentea/banzzak/Adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/greentea/banzzak/Adapter/RecyclerViewAdapter.java
@@ -1,39 +1,99 @@
 package com.greentea.banzzak.Adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.greentea.banzzak.AlarmDB.AlarmInfo;
+import com.greentea.banzzak.AlarmSettingActivity;
+import com.greentea.banzzak.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder> {
 
     Context context;
+    private final LayoutInflater layoutInflater;
+    private List<AlarmInfo> list = new ArrayList<>();
+    private OnListItemSelectedInterface mListener;
+    private OnItemClickListener itemListener = null;
+
+    final int MODIFY_REQUEST_CODE = 2;
 
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.alarm_item, parent,false);
+        return new ItemViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, final int position) {
 
+        holder.onBind(list.get(position));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Context context = v.getContext();
+                Intent intent = new Intent(context, AlarmSettingActivity.class);
+
+                intent.putExtra("alarmInfo", list.get(position));
+                ((Activity) context).startActivityForResult(intent, MODIFY_REQUEST_CODE);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return list.size();
     }
 
-    public RecyclerViewAdapter(Context context){
+    public RecyclerViewAdapter(Context context, OnListItemSelectedInterface onListItemSelectedInterface){
         this.context = context;
+        layoutInflater = LayoutInflater.from(context);
+        mListener = onListItemSelectedInterface;
     }
 
-    public class ItemViewHolder extends RecyclerView.ViewHolder {
-        public ItemViewHolder(@NonNull View itemView) {
+    public interface OnListItemSelectedInterface {
+        void onItemSelected(View v, int pos);
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(View v, int pos);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.itemListener = listener;
+    }
+
+    public void setAlarms(List<AlarmInfo> alarms){
+        list = alarms;
+//        notifyDataSetChanged();
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tv1, tv2;
+
+        public ItemViewHolder(View itemView){
             super(itemView);
+            tv1 = itemView.findViewById(R.id.tv1);
+            tv2 = itemView.findViewById(R.id.tv2);
+        }
+
+        void onBind(AlarmInfo alarmInfo){
+            tv1.setText(alarmInfo.getAlarmName());
+            tv2.setText(alarmInfo.getAlarmTime());
         }
     }
 }
--- app/src/main/java/com/greentea/banzzak/MainActivity.java
+++ app/src/main/java/com/greentea/banzzak/MainActivity.java
@@ -2,26 +2,40 @@ package com.greentea.banzzak;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.greentea.banzzak.Adapter.RecyclerViewAdapter;
+import com.greentea.banzzak.AlarmDB.AlarmInfo;
+import com.greentea.banzzak.ViewModel.AlarmViewModel;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.OnListItemSelectedInterface{
 
     final int TIME_SETTING_REQUEST_CODE = 1;
 
     FloatingActionButton addAlarmBtn;
     Intent intent;
 
+    private AlarmViewModel alarmViewModel;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addAlarmBtn = findViewById(R.id.fab_main);
+        init();
 
         addAlarmBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +49,50 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void init(){
+
+        addAlarmBtn = findViewById(R.id.fab_main);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        List<AlarmInfo> asdf = new ArrayList<>();
+
+//        for(int i=0; i<10; i++){
+            AlarmInfo aa = new AlarmInfo();
+            aa.setAlarmName(Integer.toString(0));
+            aa.setAlarmTime(Integer.toString(0));
+            asdf.add(aa);
+//        }
+
+        aa = new AlarmInfo();
+        aa.setAlarmName(Integer.toString(1));
+        aa.setAlarmTime(Integer.toString(1));
+        asdf.add(aa);
+        aa = new AlarmInfo();
+        aa.setAlarmName(Integer.toString(2));
+        aa.setAlarmTime(Integer.toString(2));
+        asdf.add(aa);
+
+        adapter = new RecyclerViewAdapter(this, this);
+        adapter.setAlarms(asdf);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+//        alarmViewModel = ViewModelProviders.of(this).get(AlarmViewModel.class);
+//        alarmViewModel.getAllAlarmInfo().observe(this, new Observer<List<AlarmInfo>>() {
+//            @Override
+//            public void onChanged(List<AlarmInfo> alarmInfos) {
+//
+//                adapter.setAlarms(alarmInfos);
+//
+//            }
+//        });
+    }
+
+    @Override
+    public void onItemSelected(View v, int pos) {
+        RecyclerViewAdapter.ItemViewHolder viewHolder = (RecyclerViewAdapter.ItemViewHolder)recyclerView.findViewHolderForAdapterPosition(pos);
     }
 }
