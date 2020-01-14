01ecde698553ee747b6467753770a100cef96b27
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-01-14 15:34:24 +0900

    splash activity 생성
    로고 xml에 집어넣기

---
+++ app/src/main/java/com/greentea/banzzak/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.greentea.banzzak;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Splash(), 2000);
+    }
+
+    private  class Splash implements Runnable{
+        @Override
+        public void run() {
+            startActivity(new Intent(getApplication(), MainActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
