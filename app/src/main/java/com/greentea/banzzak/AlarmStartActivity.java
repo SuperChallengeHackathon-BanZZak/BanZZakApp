cfe5b74523a5a6bdbaa2d66389e96d6374af7565
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-01-15 17:22:34 +0900

    알람 볼륨 조절 테스트
    알람 볼륨 조절 테스트 완료
    
    볼륨 업다운 성공
    
    처음 시작시 max volume으로 알람음 울림
    볼륨키 사용해서 인위적으로 down시킬 시 다시 음량 올리게끔 구현함

--- app/src/main/java/com/greentea/banzzak/AlarmStartActivity.java
+++ app/src/main/java/com/greentea/banzzak/AlarmStartActivity.java
@@ -1,7 +1,11 @@
 package com.greentea.banzzak;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -13,19 +17,44 @@ import com.greentea.banzzak.Service.AlarmBellService;
 
 public class AlarmStartActivity extends AppCompatActivity {
 
-    Button button;
+    Button button, upButton, downButton;
     Intent intent;
+    AudioManager audioManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_start);
 
+        audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+
         button = findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopService(intent);
+            }
+        });
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        upButton = findViewById(R.id.upButton);
+        downButton = findViewById(R.id.downButton);
+
+//        audioManager.adjustVolume(AudioManager.RINGER_MODE_NORMAL, AudioManager.FLAG_PLAY_SOUND);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0);
+
+        upButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+            }
+        });
+
+        downButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
             }
         });
 
@@ -36,7 +65,21 @@ public class AlarmStartActivity extends AppCompatActivity {
                 | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
                 | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
                 | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+    }
 
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
 
+        switch (keyCode){
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if(action == KeyEvent.ACTION_DOWN){
+                    audioManager.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+                }
+                return true;
+            default:
+                return super.dispatchKeyEvent(event);
+        }
     }
 }
